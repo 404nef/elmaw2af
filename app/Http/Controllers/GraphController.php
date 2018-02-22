2bb043cb3d548e21157e73589a904092ab1e57e0
Kilobytes22 <khaled.alaa.bakry@gmail.com>
2018-02-23 00:04:43 +0200

    Full paths

--- app/Http/Controllers/GraphController.php
+++ app/Http/Controllers/GraphController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Http\Controllers;
 use App\Street;
+use App\Transport;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;class GraphController extends Controller
 {
@@ -16,8 +17,11 @@ use Illuminate\Support\Facades\DB;class GraphController extends Controller
     public $stackarray = [];
     public $currentroute =[];
     public $bestroutes=[];
+    public $badroutes=[];
+    public $tranzites=[];
     public function startgraph(Request $request){
-        $this->constructgraph(Street::find($request->location)->street_name,Street::find($request->destination)->street_name);
+        $this->initiallocation=Street::find($request->location)->street_name;
+       $this->constructgraph($this->initiallocation,Street::find($request->destination)->street_name);
         //print_r($this->Routes);
         /*foreach ($this->Routes as $key => $value) {
               echo "<h1>Childs of".$key."<br>";
@@ -34,33 +38,57 @@ use Illuminate\Support\Facades\DB;class GraphController extends Controller
                 if($i%2==0){
                     echo "Street name :".$bestroute[$i]."<br>";
                 }else{
-                    echo "Transport number : ".$bestroute[$i];
+                    if($bestroute[$i]!=-1) {
+                        echo "Transport number : " . Transport::find($bestroute[$i])->Transport_number;
+                    }
                     echo"<br>";
                 }
             }
             echo "<br><br><br><br><br>";
         }
+
+        /*
+        $Transports = Transport::all();
+        foreach ($Transports as $transport){
+            echo $transport->Transport_number;
+            echo "<br>";
+            $khatser = $transport->streets()->get();
+            foreach ($khatser as $khat){
+                echo $khat['street_name']."    ";
+            }
+            echo "<br><br>";
+        }
+        */
     }
     public function constructgraph($location,$destination){
         $this->initialdestination=Street::where('street_name',$destination)->first();
-        if(!in_array($location,$this->currentroute)) {
+        if($location==$this->initiallocation) {
             array_push($this->currentroute, $location);
             array_push($this->currentroute, -1);
         }
         //print_r($this->currentroute);
         //print_r($this->bestroutes);
-        if($location!=$destination) {
+        if($location!=$destination&&count(array_unique($this->tranzites))<=2) {
             $this->Routes[$location] = $this->findchilds($location);
-            foreach ($this->Routes as $key => $value) {
-                if ($key == $location) {
-                    for($i =0 ; $i < count($value) ;$i+=2) {
-                        print_r($this->currentroute);
-                        $st = Street::findorFail($value[$i]);
-                        array_push($this->currentroute, $st['street_name']);
-                        array_push($this->currentroute, $value[$i+1]);
-                        $this->constructgraph($st['street_name'], $destination);
-                        array_pop($this->currentroute);
-                        array_pop($this->currentroute);
+            if(count($this->Routes[$location]>0)) {
+                foreach ($this->Routes as $key => $value) {
+                    if ($key == $location) {
+                        for ($i = 0; $i < count($value); $i += 2) {
+
+                            //print_r($this->currentroute);
+                            //echo "<br><br>";
+                            //print_r($this->currentroute);
+                            $st = Street::findorFail($value[$i]);
+                            array_push($this->currentroute, $st['street_name']);
+                            array_push($this->currentroute, $value[$i + 1]);
+                            array_push( $this->tranzites,$value[$i + 1]);
+                            $this->constructgraph($st['street_name'], $destination);
+                            array_pop($this->currentroute);
+                            array_pop($this->currentroute);
+                            array_pop($this->tranzites);
+
+
+                        }
 
                     }
                 }
@@ -69,6 +97,8 @@ use Illuminate\Support\Facades\DB;class GraphController extends Controller
             if($location==$destination) {
                 if (!in_array($this->currentroute,$this->bestroutes)) {
                     array_push($this->bestroutes, $this->currentroute);
+                    //print_r($this->tranzites);
+
                 }
             }
         }
@@ -85,14 +115,14 @@ use Illuminate\Support\Facades\DB;class GraphController extends Controller
             foreach ($transports as $transport) {
                 if ($transport->street_next_id!=0) {
                     $st = Street::find($transport->street_next_id);
-                    if( !in_array($st['street_name'],$this->currentroute)) {
+                    if( !in_array($st['street_name'],$this->currentroute) && !in_array($st['street_name'],$this->badroutes)) {
                         array_push($children, $transport->street_next_id);
                         array_push($children, $transport->transport_id);
                     }
                 }
                 if ($transport->street_prev_id!=0) {
                     $st = Street::find($transport->street_prev_id);
-                    if( !in_array($st['street_name'],$this->currentroute)) {
+                    if( !in_array($st['street_name'],$this->currentroute) &&!in_array($st['street_name'],$this->badroutes)) {
                         //echo "prev".$st['street_name']."<br>";
                         array_push($children, $transport->street_prev_id);
                         array_push($children, $transport->transport_id);
